42ae79c7b60ecad948c461b9ae30175dc49ee3ec
Jonathan Moreno <mario_morenor@hotmail.com>
2022-10-16 20:10:49 -0500

    Agregado Cors y ruta principal para testear

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const { router } = require("./routes");
 const { departments } = require("./routes/departments");
@@ -10,9 +11,14 @@ dotenv.config();
 const port = process.env.SERVER_PORT || 6969;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Server Online");
+});
+
 app.use("/api", router);
 app.use("/api", departments);
 app.use("/api", stations);
